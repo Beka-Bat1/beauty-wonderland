55c1d65c932b6253ad0ef3625dcdb531c2f918ed
beka-bat1 <bekabatmanashvili1@gmail.com>
2021-05-15 00:13:21 +0400

    n

--- src/layer/navbar/Navbar.js
+++ src/layer/navbar/Navbar.js
@@ -4,56 +4,61 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-              <li className="nav-item">
-                <a class="nav-link" href="#">
-                  Shop
-                </a>
-              </li>
-              <li className="nav-item">
-                <a class="nav-link" href="#">
-                  Gallery
-                </a>
-              </li>
-              <li className="nav-item">
-                <a class="nav-link" href="#">
-                  About
-                </a>
-              </li>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-alignment">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Shop
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Gallery
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+              </ul>
 
               <li className="nav-item">
-                <a class="navbar-brand" href="#">
+                <a className="navbar-brand" href="#">
                   Beauty Wonderland
                 </a>
               </li>
-              <li className="nav-item">
-                <a class="nav-link" href="#">
-                  My account
-                </a>
-              </li>
-              <li className="nav-item">
-                <a class="nav-link" href="#">
-                  My cart
-                </a>
-                <div className="cart__circle">1</div>
-              </li>
+
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    My account
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a class="nav-link" href="#">
+                    My cart
+                    <div className="cart__circle">1</div>
+                  </a>
+                </li>
+              </ul>
             </ul>
           </div>
 
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
+            for="btnControl"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <i class="fas fa-bars"></i>
-            {/* <span class="navbar-toggler-icon"> </span> */}
+            <i className="fas fa-bars"></i>
           </button>
         </div>
       </nav>
